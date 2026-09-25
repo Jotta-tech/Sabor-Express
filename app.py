560f2d46daf9cf3ccfb90492dc89f289de18b8e2
Jotta-tech <maciel.jptech@gmail.com>
2026-09-24 22:11:23 -0300

    adicionei dicionarios

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 
-restaurantes = ["Pizza", "Sushi"]
-
+restaurantes = [{"nome": "Praça", "categoria": "Japonesa", "ativo": False }, 
+                {"nome": "Pizza suprema", "categoria": "Italiana", "ativo": True},
+                {"nome": "Cantina", "categoria": "italiano", "ativo": False}]
 
 def exibir_nome_do_programa():
     print("""
@@ -39,8 +40,11 @@ def exibir_subtitulo(texto):
 def cadastrar_novo_restaurante():
     exibir_subtitulo("Cadastrar novos restaurantes")
     nome_do_restaurante = input("Digite o nome do restaurante que deseja cadastrar: ")
-    restaurantes.append(nome_do_restaurante)
+    categoria = input(f"Digite o nome da categoria do restaurante {nome_do_restaurante}: ")
+    dados_do_restaurante = {"nome":nome_do_restaurante, "categoria": categoria, "ativo": False}
+    restaurantes.append(dados_do_restaurante)
     print(f"O restaurante {nome_do_restaurante} foi cadastrado com sucesso!\n ")
+    
     voltar_ao_menu_principal()
     
 
@@ -48,7 +52,10 @@ def listar_restaurantes():
     exibir_subtitulo("Listando restaurantes")
 
     for restaurante in restaurantes:
-        print(f".{restaurante}")
+        nome_restaurante = restaurante["nome"]
+        categoria = restaurante["categoria"]
+        ativo = restaurante ["ativo"]
+        print(f"- {nome_restaurante} | {categoria} | {ativo}")
 
     voltar_ao_menu_principal()
 
